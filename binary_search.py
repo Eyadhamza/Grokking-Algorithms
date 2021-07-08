13d895baa901e8190a39b01cb497408050ab2f9d
EyadHamza <EyadHamza0@outlook.com>
2021-07-08 17:48:49 +0200

    binary_search.py

---
+++ binary_search.py
@@ -0,0 +1,17 @@
+def binary_search(list,searchItem):
+    lowIndex = 0
+    highIndex = len(list) - 1
+
+    while lowIndex <= highIndex:
+        midIndex = (lowIndex + highIndex)
+        guessItem = list[midIndex]
+
+        if guessItem == searchItem:
+            return midIndex
+
+        if guessItem > searchItem:
+            highIndex = midIndex - 1
+        else:
+            lowIndex = midIndex + 1
+        return None
+
