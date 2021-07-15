5f4d02d942990aef69e31537d4c35fc73a33347c
EyadHamza <EyadHamza0@outlook.com>
2021-07-15 17:58:31 +0200

    selection sort

--- algorithms.py
+++ algorithms.py
@@ -1,4 +1,4 @@
-def binary_search(list,searchItem):
+def binary_search(list, searchItem):
     lowIndex = 0
     highIndex = len(list) - 1
 
@@ -15,3 +15,20 @@ def binary_search(list,searchItem):
             lowIndex = midIndex + 1
     return None
 
+
+def findSmallest(arr):
+    smallest = arr[0]
+    smallest_index = 0
+    for i in range(1, len(arr)):
+        if arr[i] < smallest:
+            smallest = arr[i]
+            smallest_index = i
+    return smallest_index
+
+
+def selectionSort(arr):
+    newArr = []
+    for i in range(len(arr)):
+        smallest = findSmallest(arr)
+        newArr.append(arr.pop(smallest))
+    return newArr
--- tests/test_algorithms.py
+++ tests/test_algorithms.py
@@ -8,5 +8,13 @@ class TestAlgorithms(unittest.TestCase):
         my_list = [1, 3, 5, 7, 9]
 
         result = algorithms.binary_search(my_list, 3)
-        print(result)
         self.assertEqual(result, 1)
+
+        result = algorithms.binary_search(my_list, -1)
+        self.assertIsNone(result)
+
+    def test_selection_sort(self):
+        myList = [5, 3, 6, 2, 10]
+        result = algorithms.selectionSort(myList)
+
+        self.assertListEqual(result, [2, 3, 5, 6, 10])
